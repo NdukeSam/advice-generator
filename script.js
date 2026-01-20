3f5dd098333abfe8850c6f0477ea782a58cd8f37
Nduke Sam <diamondsam33@gmail.com>
2026-01-20 13:50:59 +0100

    JS script updated. Advice working

---
+++ script.js
@@ -0,0 +1,17 @@
+const fetchAdvice = async () => {
+  try {
+    const response = await fetch('https://api.adviceslip.com/advice', { cache: 'no-cache' });
+    const data = await response.json();
+    console.log(`Advice #${data.slip.id}: "${data.slip.advice}"`);
+    document.getElementById('advice-id').innerHTML = `Advice #${data.slip.id}`;
+    document.getElementById('advice-text').innerHTML = data.slip.advice
+
+  } catch (error) {
+    console.error('Error fetching advice:', error);
+  }
+
+}
+
+fetchAdvice();
+
+// document.getElementById('generate-advice').addEventListener('click', fetchAdvice);  
